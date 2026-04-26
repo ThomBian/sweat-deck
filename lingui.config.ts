57b7f726cea1f3034a1257f0f3f11f82a0a34c34
Thomas <thomas.bianchini@me.com>
2026-04-26 19:26:44 +0200

    feat(i18n): install lingui and configure cli

---
+++ lingui.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@lingui/cli';
+
+export default defineConfig({
+  sourceLocale: 'en',
+  locales: ['en', 'fr'],
+  catalogs: [
+    {
+      path: '<rootDir>/src/locales/{locale}/messages',
+      include: ['src'],
+    },
+  ],
+  format: 'po',
+  compileNamespace: 'ts',
+});
